5e51119c641a8b880ed2b00fd0d099526458b39a
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 10:38:13 +0530

    Create Pattern-31.py

---
+++ Pattern-31.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    print(" "*(i-1),(str(chr(65+n-i))+" ")*(n+1-i))
+    
+#output
+# E E E E E 
+#  D D D D 
+#   C C C 
+#    B B 
+#     A 
