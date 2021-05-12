0c9a55ee4e71ed1cc3ff04e347b7cae6b2e91fba
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 10:34:41 +0530

    Create Pattern-30.py

---
+++ Pattern-30.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    print(" "*(i-1),end="")
+    for j in range(1,n+2-i):
+        print(j,end=" ")
+    print()
+    
+ #output
+#1 2 3 4 5 
+# 1 2 3 4 
+#  1 2 3 
+#  1 2 
+#   1 
