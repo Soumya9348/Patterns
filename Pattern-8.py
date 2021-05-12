5be81f5f110701ad04938d20a686d0cff69df249
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 08:25:59 +0530

    Create Pattern-8.py

---
+++ Pattern-8.py
@@ -0,0 +1,16 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    print(chr(65+n-j),end=" ")
+  print()
+#output
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
+#J I H G F E D C B A
