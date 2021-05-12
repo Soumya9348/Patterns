8f40920b8d087317df88ad75badbb07a74c1a853
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 10:19:55 +0530

    Create Pattern-24.py

---
+++ Pattern-24.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+  print(" "*(n-i),(str(i)+" ")*i) 
+  print()
+ 
+#output
+ #   1 
+
+#   2 2 
+
+#  3 3 3 
+
+# 4 4 4 4 
+
+#5 5 5 5 5 
