a34905507987e35f8b7f39f454abde4ea45390fd
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 11:03:41 +0530

    Create Pattern-38.py

---
+++ Pattern-38.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(2*i-1,0,-1):
+        print(j,end=" ")
+    print()
+ 
+#output
+#     1 
+#   3 2 1 
+#  5 4 3 2 1 
+# 7 6 5 4 3 2 1 
+#9 8 7 6 5 4 3 2 1 
