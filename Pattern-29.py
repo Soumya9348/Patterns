684678559344d0a5d25c37bb5267cdd08eac73aa
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 10:31:47 +0530

    Create Pattern-29.py

---
+++ Pattern-29.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    print(" "*(i-1),(str(n+1-i)+" ")*(n+1-i))
+    
+#output
+#5 5 5 5 5 
+#  4 4 4 4 
+#   3 3 3 
+#    2 2 
+#     1 
