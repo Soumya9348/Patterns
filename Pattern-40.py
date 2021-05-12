445fbd896795788338d17198aa3aa111e44bac84
Soumya9348 <81823164+Soumya9348@users.noreply.github.com>
2021-05-12 11:18:51 +0530

    Create Pattern-40.py

---
+++ Pattern-40.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    for j in range(65+2*i-2,64,-1):
+        print(chr(j),end=" ")
+    print()
+    
+ #output
+#    A 
+#   C B A 
+#  E D C B A 
+# G F E D C B A 
+#I H G F E D C B A
